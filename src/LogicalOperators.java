13c3968a2735a3afcb0f247d2917260581545807
Rahela Runcan <rahela.runcan@itiviti.com>
2021-12-10 09:29:25 +0200

    Tema
    Change-Id: I5f2e70bb798af7d91e33ae12981cf928af13d9d4

--- src/LogicalOperators.java
+++ src/LogicalOperators.java
@@ -30,4 +30,126 @@ public class LogicalOperators {
         }
         System.out.println(max);
     }
+
+
+//    Creati o metoda in clasa LogicalOp, care sa primeasca un numar ca si parametru.
+//    Metoda sa verifice numarul primit, si sa afiseze in consola o numaratoare pana la 100,
+//    pornind de la numarul primit ca si parametru. Atentie la crearea tipului metodei,
+//    deoarece metoda afiseaza, nu returneaza.
+
+    public void numaratoarePanaLaOSuta(int number) {
+        if (number < 100) {
+            for (int i = number; i <= 100; i++) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("numarul trebuie sa fie <100");
+        }
+    }
+
+    //   2. Creati o metoda in clasa LogicalOp, care sa primeasca un numar ca si parametru.
+    //   Metoda sa verifice numrul primit, si sa afiseze in consola o numaratoare pana
+    //   la -100, pornind de la numarul primit ca si parametru. Atentie la crearea tipului metodei,
+    //   deoarece metoda afiseaza, nu returneaza.
+
+
+    public void numaratoarePanaLaMinusOSuta(int number) {
+        if (number > -100) {
+            for (int i = number; i >= -100; i--) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("numarul trebuie sa fie >-100");
+        }
+    }
+
+    // . Creati o metoda in clasa LogicalOp, care sa primeasca doi parametrii de tip numar.
+    // Metoda trebuie sa verifice care dintre cele doua numere este mai mare, si sa execute o
+    // numaratoare crescatoare de la cel mai mic la cel mai mare.(ex: daca int x e primul parametru
+    // iar int y  este cel de-al doilea, daca x  este mai mare decat y,  atunci numaratoarea sa fie de la y la x).
+
+
+    public void numaratoareDelaPanaLa(int numberOne, int numberTwo) {
+        if (numberOne > numberTwo) {
+            for (int i = numberTwo; i <= numberOne; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = numberOne; i <= numberTwo; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    //5. Creati o metoda in clasa LogicalOp care sa afiseze toate numerele pare de la 1 la 100.
+//6. Creati o metoda in clasa LogicalOp care sa afiseze toate numerele impare de la 1 la 100.
+    public void numerePare(int numberOne, int numberTwo) {
+        if (numberOne > numberTwo) {
+            for (int i = numberTwo; i <= numberOne; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            for (int i = numberOne; i <= numberTwo; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+
+    public void numereImpare(int numberOne, int numberTwo) {
+        if (numberOne > numberTwo) {
+            for (int i = numberTwo; i <= numberOne; i++) {
+                if (!(i % 2 == 0)) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            for (int i = numberOne; i <= numberTwo; i++) {
+                if (!(i % 2 == 0)) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+
+    //   1. Creati o metoda in clasa LogicalOp, care sa primeste un numar ca si parametru.
+    //   Metoda sa verifice numrul primit, si sa afiseze in consola o numaratoare pana la 100,
+    //   pornind de la numarul primit ca si parametru. Atentie la crearea tipului metodei,
+    //   deoarece metoda afiseaza, nu returneaza.
+
+    public void numaratoarePanaLaOSutaWhile(int number) {
+        if (number < 100) {
+            int i = number;
+            while (i <= 100) {
+                System.out.println(i);
+                i++;
+            }
+        } else {
+            System.out.println("numarul trebuie sa fie <100");
+        }
+    }
+
+    //    Creati o metoda in clasa LogicalOp, care sa primeste doi parametrii de tip numar.
+    //    Metoda trebuie sa verifice care dintre cele doua numere este mai mare, si sa execute
+    //    o numaratoare crescatoare de la cel mai mic la cel mai mare.(ex: daca int x e primul
+    //    parametru iar int y  este cel de-al doilea, daca x  este mai mare decat y,
+    //    atunci numaratoarea sa fie de la y la x).
+    public void numaratoareDelaPanaLaWhile(int numberOne, int numberTwo) {
+        if (numberOne > numberTwo) {
+            int i = numberTwo;
+            while (i <= numberOne) {
+                System.out.println(i);
+                i++;
+            }
+        } else {
+            int i = numberOne;
+            while (i <= numberTwo) {
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 //
 //        System.out.println("Hello \nRahela");
-//        System.out.println("2 + 3 =" + " " +(2 + 3));
-//        System.out.println("2 / 3 =" + " " +(2.0 / 3.0));
+//        System.out.println("2 + 3 =" + " " + (2 + 3));
+//        System.out.println("2 / 3 =" + " " + (2.0 / 3.0));
 //
-//        System.out.println("-5 + 8 * 6=" + " " +(-5 + 8 * 6));
-//        System.out.println("(55+9) % 9 =" + " " +((55+9) % 9 ));
-//        System.out.println("20 + -3*5 / 8  =" + " " +(20 + (-3*5 )/ 8 ));
-//        System.out.println(" 5 + 15 / 3 * 2 - 8 % 3  =" + " " +( 5 + 15 / 3 * 2 - 8 % 3 ));
+//        System.out.println("-5 + 8 * 6=" + " " + (-5 + 8 * 6));
+//        System.out.println("(55+9) % 9 =" + " " + ((55 + 9) % 9));
+//        System.out.println("20 + -3*5 / 8  =" + " " + (20 + (-3 * 5) / 8));
+//        System.out.println(" 5 + 15 / 3 * 2 - 8 % 3  =" + " " + (5 + 15 / 3 * 2 - 8 % 3));
 //
 //        LogicalOperators op = new LogicalOperators();
 //        Scanner scan = new Scanner(System.in);
@@ -31,34 +27,46 @@ public class Main {
 //        System.out.print("Varsta:");
 //        int varsta = scan1.nextInt();
 //        op.isEligibleToVote(varsta);
-//       Calculator calcCelsius = new Calculator();
+//        Calculator calcCelsius = new Calculator();
+//
+//        System.out.println(restulImpartirii(5, 4));
+//        System.out.println(distantaInMetrii(40));
+//        afiseazaViteza();
 
-//      System.out.println(restulImpartirii(5,4));
-//     System.out.println(distantaInMetrii(40));
-        afiseazaViteza();
-
-        // calcCelsius.checkStringInArray("are");
-        //calcCelsius.fibonaciSeries(10);
-        //    calcCelsius.CozaLozaWoza();
-        //  System.out.println("Temperatura in grade C este = " + calcCelsius.CalculCelsius(100));
-
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(15);
-        list.add(12);
-        list.add(31);
-        list.add(4);
-        list.add(7);
-        list.add(34);
-        list.add(198);
-        list.add(3);
-        list.add(2);
-        list.add(10);
-        list.add(0);
-        System.out.println(list);
+//        calcCelsius.checkStringInArray("are");
+//        calcCelsius.fibonaciSeries(10);
+//        calcCelsius.CozaLozaWoza();
+//        System.out.println("Temperatura in grade C este = " + calcCelsius.CalculCelsius(100));
+//
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(15);
+//        list.add(12);
+//        list.add(31);
+//        list.add(4);
+//        list.add(7);
+//        list.add(34);
+//        list.add(198);
+//        list.add(3);
+//        list.add(2);
+//        list.add(10);
+//        list.add(0);
+//        System.out.println(list);
 
         LogicalOperators op = new LogicalOperators();
-        op.maxNumber(list);
+//        op.maxNumber(list);
+//        op.numaratoarePanaLaOSuta(50);
+//        op.numaratoarePanaLaOSuta(500);
+//        op.numaratoarePanaLaMinusOSuta(-550);
+//        op.numaratoarePanaLaMinusOSuta(-50);
+//        op.numaratoareDelaPanaLa(12, 7);
+//        op.numaratoareDelaPanaLa(12, 7);
+//        op.numereImpare(1, 20);
+//        op.numerePare(1, 100);
+//        op.numaratoarePanaLaOSutaWhile(20);
+//        op.numaratoareDelaPanaLaWhile(1, 100);
+//        op.numerePare(1, 100);
+//        op.numerePare(1, 100);
 
 
     }
